c50b47410e7cafa086072c1960346347730e13a2
kdb04 <nikhil.srivatsa1@gmail.com>
2025-03-11 15:49:31 +0530

    spring setup and test

---
+++ backend/AttendanceManagementSystem/src/main/java/com/AttendanceManagementSystem/AttendanceManagementSystemApplication.java
@@ -0,0 +1,21 @@
+package com.AttendanceManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
+public class AttendanceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceManagementSystemApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Spring Boot is working!";
+    }
+}
---
+++ backend/AttendanceManagementSystem/src/test/java/com/AttendanceManagementSystem/AttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AttendanceManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
